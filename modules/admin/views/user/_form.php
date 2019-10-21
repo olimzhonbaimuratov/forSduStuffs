19ba9db50937612afd3a3093a8fcc9b871a8f888
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2019-10-21 23:02:57 +0600

    sdsa hello

--- modules/admin/views/user/_form.php
+++ modules/admin/views/user/_form.php
@@ -17,10 +17,10 @@ use yii\helpers\ArrayHelper;
             <div class="row">
                 <div class="col-sm-6">
                     <?= $form->field($model, 'username')->label("ID-номер") ?>
-                    <?= $form->field($model, 'first_name')->label('Имя') ?>
-                    <?= $form->field($model, 'last_name')->label('Фамилия') ?>
-                    <?= $form->field($model, 'patronymic')->label('Отчество') ?>
-                    <?= $form->field($model, 'rank')->label('Департамент') ?>
+<!--                    --><?//= $form->field($model, 'first_name')->label('Имя') ?>
+<!--                    --><?//= $form->field($model, 'last_name')->label('Фамилия') ?>
+<!--                    --><?//= $form->field($model, 'patronymic')->label('Отчество') ?>
+<!--                    --><?//= $form->field($model, 'rank')->label('Департамент') ?>
                 </div>
                 <div class="col-sm-6">
                     <?= $form->field($model, 'responsibility')->label('Должность') ?>
