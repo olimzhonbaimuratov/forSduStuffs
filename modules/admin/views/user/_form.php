ed3968695c50178b6405e9a4f6f4da073fcd38ae
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2019-10-21 22:50:03 +0600

    hello world hello world

--- modules/admin/views/user/_form.php
+++ modules/admin/views/user/_form.php
@@ -17,16 +17,16 @@ use yii\helpers\ArrayHelper;
             <div class="row">
                 <div class="col-sm-6">
                     <?= $form->field($model, 'username')->label("ID-номер") ?>
-                    <?= $form->field($model, 'first_name')->label('Имя') ?>
-                    <?= $form->field($model, 'last_name')->label('Фамилия') ?>
-                    <?= $form->field($model, 'patronymic')->label('Отчество') ?>
-                    <?= $form->field($model, 'rank')->label('Департамент') ?>
+<!--                    --><?//= $form->field($model, 'first_name')->label('Имя') ?>
+<!--                    --><?//= $form->field($model, 'last_name')->label('Фамилия') ?>
+<!--                    --><?//= $form->field($model, 'patronymic')->label('Отчество') ?>
+<!--                    --><?//= $form->field($model, 'rank')->label('Департамент') ?>
                 </div>
                 <div class="col-sm-6">
-                    <?= $form->field($model, 'responsibility')->label('Должность') ?>
-                    <?= $form->field($model, 'password')->passwordInput()->label('Пароль') ?>
-                    <?= $form->field($model, 'retypePassword')->passwordInput()->label('Повторите пароль') ?>
-                    <?= $form->field($model, 'roles')->dropDownList(\app\models\User::getDropDownList())->label('Выберите роль'); ?>
+<!--                    --><?//= $form->field($model, 'responsibility')->label('Должность') ?>
+<!--                    --><?//= $form->field($model, 'password')->passwordInput()->label('Пароль') ?>
+<!--                    --><?//= $form->field($model, 'retypePassword')->passwordInput()->label('Повторите пароль') ?>
+<!--                    --><?//= $form->field($model, 'roles')->dropDownList(\app\models\User::getDropDownList())->label('Выберите роль'); ?>
                 </div>
             </div>
             <div class="form-group">
