5a16a1d11a4a398699601d5900834d63071d7c82
AminDiyasSerikuly <160106014@stu.sdu.edu.kz>
2019-10-21 22:59:01 +0600

    sdsa hello

--- modules/admin/views/user/_form.php
+++ modules/admin/views/user/_form.php
@@ -17,7 +17,7 @@ use yii\helpers\ArrayHelper;
             <div class="row">
                 <div class="col-sm-6">
                     <?= $form->field($model, 'username')->label("ID-номер") ?>
-<!--                    --><?//= $form->field($model, 'first_name')->label('Имя') ?>
+                    <?= $form->field($model, 'first_name')->label('Имя') ?>
                     <?= $form->field($model, 'last_name')->label('Фамилия') ?>
                     <?= $form->field($model, 'patronymic')->label('Отчество') ?>
                     <?= $form->field($model, 'rank')->label('Департамент') ?>
